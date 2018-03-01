d02f39c36728956e1b15ccc0b838cf3a4a38eb59
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-03-01 17:21:11 +0530

    errors resolved

--- game/views.py
+++ game/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Room
 
-# Create your views here.
+
+@login_required
+def index(request):
+    rooms = Room.objects.order_by("title")
+
+    # Render that in the index template
+    return render(request, "index.html", {
+        "rooms": rooms,
+    })
--- quizzie/settings.py
+++ quizzie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,33 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
+
+
+NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS = getattr(settings, 'NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS', True)
+
+MSG_TYPE_MESSAGE = 0  # For standard messages
+MSG_TYPE_WARNING = 1  # For yellow messages
+MSG_TYPE_ALERT = 2  # For red & dangerous alerts
+MSG_TYPE_MUTED = 3  # For just OK information that doesn't bother users
+MSG_TYPE_ENTER = 4  # For just OK information that doesn't bother users
+MSG_TYPE_LEAVE = 5  # For just OK information that doesn't bother users
+
+MESSAGE_TYPES_CHOICES = getattr(settings, 'MESSAGE_TYPES_CHOICES', (
+    (MSG_TYPE_MESSAGE, 'MESSAGE'),
+    (MSG_TYPE_WARNING, 'WARNING'),
+    (MSG_TYPE_ALERT, 'ALERT'),
+    (MSG_TYPE_MUTED, 'MUTED'),
+    (MSG_TYPE_ENTER, 'ENTER'),
+    (MSG_TYPE_LEAVE, 'LEAVE')))
+
+MESSAGE_TYPES_LIST = getattr(settings, 'MESSAGE_TYPES_LIST',
+                             [MSG_TYPE_MESSAGE,
+                              MSG_TYPE_WARNING,
+                              MSG_TYPE_ALERT,
+                              MSG_TYPE_MUTED,
+                              MSG_TYPE_ENTER,
+                              MSG_TYPE_LEAVE])
